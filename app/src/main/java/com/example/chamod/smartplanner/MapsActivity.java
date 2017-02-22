c6837df2f5c048cdb4685f8e2325fb727d2a483f
chamod <chamod.14@cse.mrt.ac.lk>
2017-02-22 12:35:16 +0530

    Google places api added

--- app/src/main/java/com/example/chamod/smartplanner/MapsActivity.java
+++ app/src/main/java/com/example/chamod/smartplanner/MapsActivity.java
@@ -1,18 +1,44 @@
 package com.example.chamod.smartplanner;
 
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 0;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 0;
+
     private GoogleMap mMap;
+
+    Marker marker;
+
+    TextView textViewSelectedLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +48,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        textViewSelectedLocation=(TextView)findViewById(R.id.txtViewSelectedLocation);
+
+
+
+
     }
 
 
@@ -39,8 +71,203 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng colombo = new LatLng(6.9271,79.8612);
-        mMap.addMarker(new MarkerOptions().position(colombo).title("Marker in Sydney"));
+        LatLng colombo = new LatLng(6.9271, 79.8612);
+        marker=mMap.addMarker(new MarkerOptions()
+                .position(colombo)
+                .title("Marker in Colombo")
+                .draggable(true)
+        );
         mMap.moveCamera(CameraUpdateFactory.newLatLng(colombo));
+        // Zoom out to zoom level 10, animating with a duration of 1 seconds.
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(15), 1000, null);
+
+//set address
+        textViewSelectedLocation.setText(getAddress(6.9271, 81.8612));
+
+
+//        set map control buttons
+        UiSettings uiSettings = googleMap.getUiSettings();
+
+        uiSettings.setZoomControlsEnabled(true);
+        uiSettings.setRotateGesturesEnabled(false);
+        uiSettings.setIndoorLevelPickerEnabled(true);
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            requestFineLocationPermission();
+            requestCoarseLocationPermission();
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+
+//                google places
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+
+                LatLng p = new LatLng(place.getLatLng().latitude, place.getLatLng().longitude);
+                marker.setPosition(place.getLatLng());
+                marker.setTitle("Marker in "+place.getName());
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(place.getLatLng()));
+
+                // Zoom out to zoom level 10, animating with a duration of 1 seconds.
+                mMap.animateCamera(CameraUpdateFactory.zoomTo(10), 1000, null);
+
+                textViewSelectedLocation.setText(place.getAddress());
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("tag", "An error occurred: " + status);
+            }
+        });
+
+    }
+
+
+    private String getAddress(double lat,double lng){
+        String add = "";
+        Geocoder geoCoder = new Geocoder(getBaseContext(), Locale.getDefault());
+        try {
+            List<Address> addresses =
+                    geoCoder.getFromLocation(lat, lng, 1);
+
+            Address obj=addresses.get(0);
+
+            add=obj.getAddressLine(0);
+            add = add + "," + obj.getCountryName();
+
+//            add = add + "\n" + obj.getCountryCode();
+//            add = add + "\n" + obj.getAdminArea();
+//            add = add + "\n" + obj.getPostalCode();
+//            add = add + "\n" + obj.getSubAdminArea();
+//            add = add + "\n" + obj.getLocality();
+//            add = add + "\n" + obj.getSubThoroughfare();
+//
+            return add;
+//            if (addresses.size() > 0) {
+//                for (int i = 0; i < addresses.get(0).getMaxAddressLineIndex(); i++)
+//                    filterAddress += addresses.get(0).getAddressLine(i) + ", ";
+//            }
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (Exception e2) {
+            // TODO: handle exception
+            e2.printStackTrace();
+        }
+        return add;
+    }
+
+    /**
+     * Requests the ACCESS_FINE_LOCATION permission.
+     * If the permission has been denied previously, a dialog will prompt the user to grant the
+     * permission, otherwise it is requested directly.
+     */
+    private void requestFineLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+            new AlertDialog.Builder(MapsActivity.this)
+                    .setTitle("Warning!")
+                    .setMessage("Do you want to track this device, Please allow the permission request")
+                    .setCancelable(false)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //re-request
+                            ActivityCompat.requestPermissions(MapsActivity.this,
+                                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+                        }
+                    })
+                    .show();
+        } else {
+            // READ_PHONE_STATE permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+    }
+
+
+    /**
+     * Requests the ACCESS_COARSE_LOCATION permission.
+     * If the permission has been denied previously, a dialog will prompt the user to grant the
+     * permission, otherwise it is requested directly.
+     */
+    private void requestCoarseLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION)) {
+            // Provide an additional rationale to the user if the permission was not granted
+            // and the user would benefit from additional context for the use of the permission.
+            // For example if the user has previously denied the permission.
+            new AlertDialog.Builder(MapsActivity.this)
+                    .setTitle("Warning!")
+                    .setMessage("Do you want to track this device, Please allow the permission request")
+                    .setCancelable(false)
+                    .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            //re-request
+                            ActivityCompat.requestPermissions(MapsActivity.this,
+                                    new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                                    MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+                        }
+                    })
+                    .show();
+        } else {
+            // READ_PHONE_STATE permission has not been granted yet. Request it directly.
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                    MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+
+                    if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return;
+                    }
+                    mMap.setMyLocationEnabled(true);
+
+                } else {
+
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+//                    Toast.makeText(LoggedActivity.this,"Please provide location permissions to track the device...!",Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request
+        }
     }
 }
