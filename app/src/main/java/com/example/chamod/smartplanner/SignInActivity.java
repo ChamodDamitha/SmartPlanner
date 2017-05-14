26cb6e8afe5b338ceb35e4015a84360f86a5e04c
chamod <chamod.14@cse.mrt.ac.lk>
2017-05-14 12:53:09 +0530

    permission check added

--- app/src/main/java/com/example/chamod/smartplanner/SignInActivity.java
+++ app/src/main/java/com/example/chamod/smartplanner/SignInActivity.java
@@ -1,8 +1,13 @@
 package com.example.chamod.smartplanner;
 
+import android.*;
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.TelephonyManager;
@@ -18,7 +23,9 @@ import com.android.volley.toolbox.StringRequest;
 import com.example.chamod.smartplanner.Database.UserDB;
 import com.example.chamod.smartplanner.Handlers.AppController;
 import com.example.chamod.smartplanner.Models.User;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.auth.api.signin.internal.SignInHubActivity;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -28,12 +35,21 @@ import java.util.Map;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private GoogleApiClient googleApiClient=null;
+    private GoogleSignInOptions signInOptions;
+
+
     UserDB userDB;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        requestPermissions();
+
+    }
+
+    private void initialize(){
         userDB=UserDB.getInstance(this);
 
         if(userDB.getUser()!=null){
@@ -41,6 +57,48 @@ public class SignInActivity extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
+    private void requestPermissions(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS},
+                    Constants.MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            return;
+        }
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_SMS) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_SMS},
+                    Constants.MY_PERMISSIONS_REQUEST_READ_SMS);
+            return;
+        }
+        if(ActivityCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.SEND_SMS},
+                    Constants.MY_PERMISSIONS_REQUEST_SEND_SMS);
+            return;
+        }
+
+        initialize();
+    }
+
+
+    //    ...............................Permission response..........................................
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case Constants.MY_PERMISSIONS_REQUEST_READ_CONTACTS:{
+                requestPermissions();
+            }
+            case Constants.MY_PERMISSIONS_REQUEST_READ_SMS:{
+                requestPermissions();
+            }
+            case Constants.MY_PERMISSIONS_REQUEST_SEND_SMS:{
+                requestPermissions();
+            }
+        }
+    }
+
 
     public void Signin(View view){
 
@@ -51,7 +109,7 @@ public class SignInActivity extends AppCompatActivity {
         progressDialog.setMessage("Connecting to server");
         progressDialog.show();
 
-
+//google signin
         final String email="chamod@gmail.com";
         final String name="chamod";
 
@@ -77,7 +135,7 @@ public class SignInActivity extends AppCompatActivity {
                         User user=new User(email,name,mPhoneNumber);
                         userDB.addUser(user);
 
-                       startActivity(new Intent(SignInActivity.this,NavigaterActivity.class));
+                        startActivity(new Intent(SignInActivity.this,NavigaterActivity.class));
                     }
                     Toast.makeText(SignInActivity.this,response.get("msg").toString(),Toast.LENGTH_LONG).show();
                 } catch (JSONException e) {
@@ -96,4 +154,6 @@ public class SignInActivity extends AppCompatActivity {
 
         AppController.getInstance(this).addToRequestQueue(jsonObjectRequest);
     }
+
+
 }
